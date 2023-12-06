1d19cf06a684eb6ff9b9f1c3f39c3fe8a2bc7b65
alonso-grj <152428045+alonso-grj@users.noreply.github.com>
2023-12-05 18:59:36 -0600

    Update main.cpp
    step 5

--- main.cpp
+++ main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 
 
 void PrintMenu(const string playlistTitle) {
-   /* Type your code here */
+   cout<< playlistTitle << " PLAYLIST MENU"<<endl;
+   cout<<"a - Add song"<<endl;
+   cout<<"d - Remove song"<<endl;
+   cout<<"c - Change position of song"<<endl;
+   cout<<"s - Output songs by specific artist"<<endl;
+   cout<<"t - Output total time of playlist (in seconds)"<<endl;
+   cout<<"o - Output full playlist"<<endl;
+   cout<<"q - Quit"<<endl<<endl;
+
+   cout<<"Choose an option:"<<endl
    
 }
 

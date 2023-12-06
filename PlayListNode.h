7c8295aa2d2568f964343a4728063357000f0ee7
alonso-grj <152428045+alonso-grj@users.noreply.github.com>
2023-12-05 18:35:04 -0600

    Update PlayListNode.h
    definition of variables

--- PlayListNode.h
+++ PlayListNode.h
@@ -1,12 +1,34 @@
 //
 // Created by Emilio Munoz on 12/5/23.
-//
-
 #ifndef ZYLAB_28_15_PLAYLISTNODE_H
 #define ZYLAB_28_15_PLAYLISTNODE_H
 
-
 class PlayListNode {
+public:
+    PlaylistNode();  
+    PlaylistNode(std::string uniqueID, std::string songName, std::string artistName, int songLength);  
+
+    std::string GetID() const;
+    std::string GetSongName() const;
+    std::string GetArtistName() const;
+    int GetSongLength() const;
+    PlaylistNode* GetNext() const;
+
+    // Mutators
+    void InsertAfter(PlaylistNode* nodePtr);
+    void SetNext(PlaylistNode* nodePtr);
+
+    // Other
+    void PrintPlaylistNode() const;
+
+private:
+    std::string uniqueID;
+    std::string songName;
+    std::string artistName;
+    int songLength;
+    PlaylistNode* nextNodePtr;
+
+
 
 };
 

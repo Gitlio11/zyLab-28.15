ef68835d31c61b36bba56eb38386b4ee2bf15694
Gitlio11 <eamunoz@smu.edu>
2023-12-05 18:31:51 -0600

    Created class

---
+++ PlayListNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Emilio Munoz on 12/5/23.
+//
+
+#include "PlayListNode.h"
---
+++ PlayListNode.h
@@ -0,0 +1,14 @@
+//
+// Created by Emilio Munoz on 12/5/23.
+//
+
+#ifndef ZYLAB_28_15_PLAYLISTNODE_H
+#define ZYLAB_28_15_PLAYLISTNODE_H
+
+
+class PlayListNode {
+
+};
+
+
+#endif //ZYLAB_28_15_PLAYLISTNODE_H
